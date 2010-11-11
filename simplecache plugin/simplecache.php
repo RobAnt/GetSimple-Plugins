3b816cad5f8ccbcf31fd854ef0b0afd1b1477ecb
Rob Antonishen <rob.antonishen@gmail.com>
2010-11-10 23:45:27 -0500

    Cleaned up code. Added backend page.

--- simplecache plugin/simplecache.php
+++ simplecache plugin/simplecache.php
@@ -7,27 +7,29 @@ Author: Rob Antonishen
 Author URI: http://ffaat.poweredbyclear.com/
 */
 
-# get correct id for plugin
+// get correct id for plugin
 $thisfile=basename(__FILE__, ".php");
 
-# register plugin
+// register plugin
 register_plugin(
 	$thisfile, 
 	'SimpleCache', 	
-	'0.1', 		
+	'0.2', 		
 	'Rob Antonishen',
 	'http://ffaat.poweredbyclear.com/', 
 	'A simple cache for Get Simple',
-	'plugin',
-	''  
+	'pages',
+	'simplecache_manage'  
 );
 
-# activate filter
+// activate filter
 add_action('changedata-save','simplecache_flushpage'); 
 add_action('index-pretemplate','simplecache_pagestart'); 
 add_action('index-posttemplate','simplecache_pageend'); 
+add_action('pages-sidebar','createSideMenu',array($thisfile,'SimpleCache Status'));
 
-# functions
+// functions
+
 /* clear the simplecache directory */
 function simplecache_flush() 
 {
@@ -56,12 +58,13 @@ function simplecache_flush()
 }
 
 /* flush one page from cache when edited */
-function simplecache_flushpage() 
+function simplecache_flushpage($page=NULL) 
 {
   $simplecache_dir = GSDATAOTHERPATH.'simplecache_cache/';
+  if ($page === null) $page = $_POST['post-id'];
   if (is_dir($simplecache_dir))
   {
-    $simplecache_file = $simplecache_dir . $_POST['post-id'] . ".cache";
+    $simplecache_file = $simplecache_dir . $page . ".cache";
     if (file_exists($simplecache_file)) 
     {
       unlink($simplecache_file) or exit('Unable to clean up the folder .../data/other/simplecache_cache, check folder content privileges.');
@@ -118,6 +121,78 @@ function simplecache_pageend()
     }
     // Send the output to the browser
     ob_end_flush();
+  }
+}
+
+function simplecache_manage()
+{
+  echo '<label>SimpleCache</label><br/><br/>';
+
+  $simplecache_dir = GSDATAOTHERPATH.'simplecache_cache/';
+  if (is_dir($simplecache_dir))
+  {
+    // cache flush requested
+    if ((isset($_GET['cache_flush'])) && ($_GET['cache_flush'] == "Y"))
+    {
+      simplecache_flush();
+    }
+ 
+    // single cache file delete requested
+    if (isset($_GET['cache_flushpage']))
+    {
+      simplecache_flushpage($_GET['cache_flushpage']);
+    }
+        
+    $pcount = 0;
+    $dir_handle = @opendir($simplecache_dir) or exit('Unable to open the folder .../data/other/simplecache_cache, check the folder privileges.');
+    $filenames = array();
+    
+    while ($filename = readdir($dir_handle))
+    {
+      $filenames[] = $filename;
+    }
+
+    echo '<table class="edittable highlight paginate">';   
+    echo '<tr id="tr-header" >';
+    echo '<th width="35%" ><b>Cache File</b></th>';
+    echo '<th><span>Created On</span></th>';
+    echo '<th width="10%"></th>';
+    echo "</tr>\n";       
+    
+    if (count($filenames) != 0)
+    {
+      foreach ($filenames as $file) 
+      {
+        if (!($file == '.' || $file == '..' || is_dir($simplecache_dir.$file) || $file == '.htaccess'))
+        {
+          $file = substr($file, 0, -6); //strip the extension
+          echo '<tr id="tr-' . $pcount . '" >';
+          echo '<td width="35%" ><b>' . $file . '</b></td>';
+          echo '<td>' . date("F d Y H:i:s", filemtime($simplecache_dir.$file.".cache")) . '</td>';
+          echo '<td width="10%"><b><a href="' . $_SERVER["REQUEST_URI"] . '&cache_flushpage=' . $file . '">Delete</a></b></td>';
+          echo "</tr>\n";   
+          
+          $pcount++;     
+        }
+      }
+    }
+    closedir($dir_handle);
+    
+    if($pcount == 0)
+    {
+      echo "<tr><td>No Cached files</td></tr>\n";
+    }
+  
+    echo "</table>\n";
+
+    if($pcount != 0)
+    {
+      echo '<br/><a href="'.$_SERVER["REQUEST_URI"].'&cache_flush=Y">Delete all cache files</a>';
+    }
+  } 
+  else 
+  {
+    echo "Folder .../data/other/simplecache_cache doesn't exist!";
   }
 }
 ?>

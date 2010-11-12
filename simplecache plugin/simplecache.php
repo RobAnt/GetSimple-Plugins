b851587db0e37e9030338e434a9f28e46498679a
Rob Antonishen <rob.antonishen@gmail.com>
2010-11-12 14:09:12 -0500

    v0.4 - added cache disable option and time based stuff.  PLuss a bunch of code rearranging.

--- simplecache plugin/simplecache.php
+++ simplecache plugin/simplecache.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: SimpleCache
 Description: a simple cache
-Version: 0.3
+Version: 0.4
 Author: Rob Antonishen
 Author URI: http://ffaat.poweredbyclear.com/
 */
@@ -14,7 +14,7 @@ $thisfile=basename(__FILE__, ".php");
 register_plugin(
 	$thisfile, 
 	'SimpleCache', 	
-	'0.3', 		
+	'0.4', 		
 	'Rob Antonishen',
 	'http://ffaat.poweredbyclear.com/', 
 	'A simple cache for Get Simple',
@@ -26,14 +26,91 @@ register_plugin(
 add_action('changedata-save','simplecache_flushpage'); 
 add_action('index-pretemplate','simplecache_pagestart'); 
 add_action('index-posttemplate','simplecache_pageend'); 
-add_action('pages-sidebar','createSideMenu',array($thisfile,'SimpleCache Status'));
+add_action('pages-sidebar','createSideMenu',array($thisfile,'SimpleCache'));
 
+// global vars
+$simplecache_conf = simplecache_loadconf();
 
-// functions
+/***********************************************************************************
+*
+* Hook functions
+*
+***********************************************************************************/
+
+/* start of page */
+function simplecache_pagestart() 
+{
+  global $simplecache_conf;
+  
+  //caching enabled?
+  if($simplecache_conf['enabled']=="Y")
+  {  
+    // check for pages to not cache
+    if(!in_array(return_page_slug(), array_merge(array("404"), $simplecache_conf['nocache'])))
+    {  
+      $simplecache_dir = GSDATAOTHERPATH.'simplecache_cache/';
+      if (is_dir($simplecache_dir))
+      {
+        $simplecache_file = $simplecache_dir .  md5(return_page_slug()) . ".cache";
+        if (file_exists($simplecache_file)) 
+        {
+          if (($simplecache_conf['ttl'] == 0) || ((time() - $simplecache_conf['ttl']*60) < filemtime($simplecache_file)))
+          {
+            // use cached file
+            include($simplecache_file); 
+            // and stop running scripts
+            exit;
+          }
+        }
+      }
+
+      if(!ob_start("ob_gzhandler")) ob_start();
+    }
+  }
+}
+
+/* end of page */
+function simplecache_pageend() 
+{
+  global $simplecache_conf;
+  
+  //caching enabled?
+  if($simplecache_conf['enabled']=="Y")
+  {
+    // check for pages to not cache
+    if(!in_array(return_page_slug(), array_merge(array("404"), $simplecache_conf['nocache'])))
+    {
+      $simplecache_dir = GSDATAOTHERPATH.'simplecache_cache/';
+      if (is_dir($simplecache_dir)==false)
+      {
+        mkdir($simplecache_dir, 0755) or exit('Unable to create .../data/other/simplecache_cache folder, check GetSimple privileges.');
+      }
+  
+      if (is_dir($simplecache_dir))
+      {
+        $simplecache_file = $simplecache_dir .  md5(return_page_slug()) . ".cache";
+        // open the cache file for writing
+        $fp = fopen($simplecache_file, 'w') or exit('Unable to save ' . $simplecache_file . ', check GetSimple privileges.');
+        // save the contents of output buffer to the file
+        fwrite($fp, ob_get_contents());
+        // close the file
+        fclose($fp);
+      }
+      // Send the output to the browser
+      ob_end_flush();
+    }
+  }
+}
+
+/***********************************************************************************
+*
+* Helper functions
+*
+***********************************************************************************/
 
 /* clear the simplecache directory */
 function simplecache_flushall() 
-{
+{ 
   $simplecache_dir = GSDATAOTHERPATH.'simplecache_cache/';
   if (is_dir($simplecache_dir))
   {
@@ -58,7 +135,7 @@ function simplecache_flushall()
   }
 }
 
-/* flush one page from cache when edited - pass in md5 hash of page slug*/
+/* flush one page from cache when edited - pass in md5 hash of page slug to specify a page otherwise flushes the edited page in the back end */
 function simplecache_flushpage($page=NULL) 
 {
   $simplecache_dir = GSDATAOTHERPATH.'simplecache_cache/';
@@ -70,59 +147,6 @@ function simplecache_flushpage($page=NULL)
     {
       unlink($simplecache_file) or exit('Unable to clean up the folder .../data/other/simplecache_cache, check folder content privileges.');
     }
-  }
-}
-
-/* start of page */
-function simplecache_pagestart() 
-{
-  // check for pages to not cache
-  if(!in_array(return_page_slug(), array("404")))
-  {  
-    $simplecache_dir = GSDATAOTHERPATH.'simplecache_cache/';
-    if (is_dir($simplecache_dir))
-    {
-      $simplecache_file = $simplecache_dir .  md5(return_page_slug()) . ".cache";
-      if (file_exists($simplecache_file)) 
-      {
-        // use cached file
-        include($simplecache_file); 
-        // and stop running scripts
-        exit;
-      }
-    }
-
-    if(!ob_start("ob_gzhandler")) ob_start();
-  }
-}
-
-/* end of page */
-function simplecache_pageend() 
-{
-  // get config variables
-  $config = simplecache_getconf(); 
-
-  // check for pages to not cache
-  if(!in_array(return_page_slug(), array_merge(array("404"), $config['nocache'])))
-  {
-    $simplecache_dir = GSDATAOTHERPATH.'simplecache_cache/';
-    if (is_dir($simplecache_dir)==false)
-    {
-      mkdir($simplecache_dir, 0755) or exit('Unable to create .../data/other/simplecache_cache folder, check GetSimple privileges.');
-    }
-  
-    if (is_dir($simplecache_dir))
-    {
-      $simplecache_file = $simplecache_dir .  md5(return_page_slug()) . ".cache";
-      // open the cache file for writing
-      $fp = fopen($simplecache_file, 'w') or exit('Unable to save ' . $simplecache_file . ', check GetSimple privileges.');
-      // save the contents of output buffer to the file
-      fwrite($fp, ob_get_contents());
-      // close the file
-      fclose($fp);
-    }
-    // Send the output to the browser
-    ob_end_flush();
   }
 }
 
@@ -165,17 +189,38 @@ function simplecache_pagelist() {
   return($pagelist); 
 }
 
-
-/* get config settings */
-function simplecache_getconf()
+/* save config settings to file*/
+function simplecache_saveconf()
 {
+  global $simplecache_conf;
+  
+  $configfile=GSDATAOTHERPATH."simplecache.xml";
+  
+  $xmlstr = '<?xml version=\'1.0\'?><simplecachesettings>';
+  $xmlstr .= "<enabled>" . $simplecache_conf['enabled'] . "</enabled>";  
+  $xmlstr .= "<ttl>" . $simplecache_conf['ttl'] . "</ttl>";
+  $xmlstr .= "<nocache>";
+  foreach ($simplecache_conf['nocache'] as $slug)
+    $xmlstr .= "<slug>" . $slug . "</slug>";
+  $xmlstr .= "</nocache>";
+  $xmlstr .= "</simplecachesettings>";
+  
+  $fp = fopen($configfile, 'w') or exit('Unable to save ' . $configfile . ', check GetSimple privileges.');
+  // save the contents of output buffer to the file
+  fwrite($fp, $xmlstr);
+  // close the file
+  fclose($fp);
+}
+
+/* get config settings from file */
+function simplecache_loadconf()
+{ 
   $vals=array();
   $configfile=GSDATAOTHERPATH."simplecache.xml";
   if (!file_exists($configfile))
   {
     //default settings
-    $xmlstr = "<?xml version='1.0'?><simplecachesettings><ttl>60</ttl><nocache></nocache></simplecachesettings>";
- 
+    $xmlstr = "<?xml version='1.0'?><simplecachesettings><enabled>Y</enabled><ttl>60</ttl><nocache></nocache></simplecachesettings>";
     $fp = fopen($configfile, 'w') or exit('Unable to save ' . $configfile . ', check GetSimple privileges.');
     // save the contents of output buffer to the file
     fwrite($fp, $xmlstr);
@@ -183,13 +228,16 @@ function simplecache_getconf()
     fclose($fp);
   }
 
+  $fp = @fopen($configfile,"r");
   $xmlvals = getXML($configfile);
-
+  fclose($fp);
+  
   $node = $xmlvals->children();
 
+  $vals['enabled'] = (string)$node->enabled;  
   $vals['ttl'] = (int)(string)$node->ttl;  
-  $subnode = $node->nocache->children();
-  
+
+  $subnode = $node->nocache->children(); 
   $vals['nocache'] = array();
   foreach($subnode->slug as $slug) 
     $vals['nocache'][] = (string)$slug;
@@ -197,46 +245,83 @@ function simplecache_getconf()
   return($vals);
 }
 
-/* set config settings */
-function simplecache_setconf($vals)
-{
-  $configfile=GSDATAOTHERPATH."simplecache.xml";
-  $xmlvals = xml_encode($vals , false, "simplecache-config");
 
-  $fp = fopen($configfile, 'w') or exit('Unable to save ' . $configfile . ', check GetSimple privileges.');
-  // save the contents of output buffer to the file
-  fwrite($fp, $xmlvals);
-  // close the file
-  fclose($fp);
-}
-
-
+/***********************************************************************************
+*
+* Backend management page
+*
+***********************************************************************************/
 function simplecache_manage()
 {
-  // get config variables
-  $config = simplecache_getconf(); 
- 
-  echo '<label>SimpleCache</label><br/><br/>';
-
+  global $simplecache_conf;
+   
   $simplecache_dir = GSDATAOTHERPATH.'simplecache_cache/';
   if (is_dir($simplecache_dir))
   {
+    $message = "";
+
+    /********** Display backend page ***********/    
+    // display cache page data
+    $pagearray = simplecache_pagelist();
+    
+    /********** post/get checks **********/
     // cache flush requested
     if ((isset($_GET['cache_flush'])) && ($_GET['cache_flush'] == "Y"))
     {
       simplecache_flushall();
-    }
- 
+      $message = "All cache files deleted.";
+    } 
     // single cache file delete requested
-    if (isset($_GET['cache_flushpage']))
+    elseif (isset($_GET['cache_flushpage']))
     {
       simplecache_flushpage($_GET['cache_flushpage']);
+      $message = "Cache file deleted.";    
     }
         
-        
-    // display cache page data
-    $pagearray = simplecache_pagelist();
-    
+    // update of per page setings
+    if (isset($_POST['submit_perpage']))
+    {
+      $nocache=array();
+      foreach ($pagearray as $pagedata)
+      {
+        if (!in_array($pagedata['slug'], $_POST['cache']))
+        {
+          $nocache[] = $pagedata['slug'];
+          if (!in_array($pagedata['slug'], $simplecache_conf['nocache']))
+            simplecache_flushpage(md5($pagedata['slug']));
+        }
+      }
+      $simplecache_conf['nocache'] = $nocache;
+      simplecache_saveconf();
+      $message = "Updated per-page caching.";    
+    }
+
+    // update of general settings
+    if (isset($_POST['submit_settings']))
+    {
+      if (isset($_POST['enabled']))
+      {
+        $simplecache_conf['enabled']="Y";
+      } 
+      else
+      {
+        $simplecache_conf['enabled']="N";
+      }
+      
+      if ((isset($_POST['ttl'])) && is_numeric($_POST['ttl']))
+        $simplecache_conf['ttl']=min(1400, max(0, (int)$_POST['ttl']));
+
+      simplecache_saveconf();
+      $message = "Updated settings.";    
+    }
+
+    //disabled cache warning
+    if ($simplecache_conf['enabled']=="N") echo '<div style="display: block;" class="updated">SimpleCache page caching is currently disabled!</div>';
+
+    if ($message!="") echo '<div style="display: block;" class="updated">' . $message . '</div>';
+
+        echo '<label>SimpleCache Status</label><br/><br/>';
+       
     $pcount = 0;
     $dir_handle = @opendir($simplecache_dir) or exit('Unable to open the folder .../data/other/simplecache_cache, check the folder privileges.');
     $filenames = array();
@@ -247,7 +332,7 @@ function simplecache_manage()
     }
     
     //set up form to disable specific page caching
-    echo '<form class="manyinputs" action="load.php?id=simplecache" method="post">';
+    echo '<form name="status" class="manyinputs" action="load.php?id=simplecache" method="post">';  
     echo '<table class="edittable highlight paginate">';   
     echo '<tr id="tr-header" >';
     echo '<th align="center" width="30px">Cache</th>';
@@ -262,8 +347,8 @@ function simplecache_manage()
       foreach ($pagearray as $pagedata) 
       {
         echo '<tr>';
-        echo '<td align="center" width="30px"><input type= "checkbox" name = "cache" value = "' . $pagedata['slug'] . '"';
-        if (!in_array($pagedata['slug'], $config['nocache'])) echo ' checked';
+        echo '<td align="center" width="30px"><input type="checkbox" name="cache[]" value = "' . $pagedata['slug'] . '"';
+        if (!in_array($pagedata['slug'], $simplecache_conf['nocache'])) echo ' checked';
         echo '></td>';
         echo '<td width="50%" >' . $pagedata['title'] . '</td>';
 
@@ -276,7 +361,7 @@ function simplecache_manage()
         }
         else
         {
-          echo '<td>Not in cache.</td><td width="10px"></td>';
+          echo '<td>Page not cached.</td><td width="10px"></td>';
         }
 
         echo "</tr>\n";   
@@ -285,7 +370,7 @@ function simplecache_manage()
     }
     closedir($dir_handle); 
     echo "</table>\n";
-    echo "<input name='submit' class='submit' type='submit' value='Update per-page settings'>\n";
+    echo "<input name='submit_perpage' class='submit' type='submit' value='Update per-page caching'>\n";
 
     //delete all cached pages button
     if($pcount != 0)
@@ -294,7 +379,19 @@ function simplecache_manage()
       echo 'onclick="window.location.href=' . "'" . $_SERVER["REQUEST_URI"] . "&cache_flush=Y'" . '">';    
     }
     echo "</form>\n";
-    
+
+    // general plugin settings    
+    echo '<br/><br/><label>SimpleCache Settings</label><br/><br/>';
+    echo '<form name="settings" class="manyinputs" action="load.php?id=simplecache" method="post">';  
+    echo '<input type="checkbox" name="enabled" value="Y"';
+    if ($simplecache_conf['enabled'] == "Y") echo ' checked';
+    echo '>&nbsp; SimpleCache page caching enabled.<br /><br />';
+    echo '<input name="ttl" type="text" size="4" value="'.$simplecache_conf['ttl'] .'">&nbsp;&nbsp;&nbsp;Life of cached pages (minutes).  Enter 0 for no time based expiry.<br /><br />';
+    echo "<input name='submit_settings' class='submit' type='submit' value='Save Settings'>\n";    
+    echo '</form>';
+  
+  
+  
   } 
   else 
   {

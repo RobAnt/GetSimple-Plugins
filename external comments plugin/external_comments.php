2588865110612d3b0536fe30f336fdf55594766c
Rob Antonishen <rob.antonishen@gmail.com>
2011-04-12 15:49:03 -0400

    -Modified to work with Facebook -Modified to allow parameter overrides to the return_external_comments() and get_external_comments() calls

--- external comments plugin/external_comments.php
+++ external comments plugin/external_comments.php
@@ -1,8 +1,10 @@
 <?php
 /*
 Plugin Name: External Comments
-Description: Provides external comments support on pages (Disqus or IntenseDebate)
-Version: 0.3
+Description: Provides external comments support on pages (Disqus IntenseDebate, or Facebook connect)
+Extended to specify optionally override the page information so this call can be used
+in external plugins like the News manager.
+Version: 0.5
 Author: Rob Antonishen
 Author URI: http://ffaat.poweredbyclear.com/
 */
@@ -14,7 +16,7 @@ $thisfile=basename(__FILE__, ".php");
 register_plugin(
     $thisfile, 
     'External Comments',     
-    '0.3',         
+    '0.5',         
     'Rob Antonishen',
     'http://ffaat.poweredbyclear.com', 
     'Provides external comments support',
@@ -22,12 +24,24 @@ register_plugin(
     'external_comments_config'  
 );
 
-# activate filter
-add_action('plugins-sidebar','createSideMenu',array($thisfile,'External Comments'));
-add_filter('content','external_comments_display'); 
-
 # global vars
 $external_comments_conf = external_comments_loadconf();
+
+# activate filter
+add_filter('content','external_comments_display'); 
+
+add_action('plugins-sidebar','createSideMenu',array($thisfile,'External Comments'));
+add_action('theme-header','external_comments_header',array());
+
+/* Inject meta code for facebook admin into header as necessary */
+function external_comments_header(){
+  global $external_comments_conf, $data_index;
+
+  if (strpos($data_index->content, '(% external_comments %)') !== false && $external_comments_conf['provider'] == "facebook")
+  {
+    echo '<meta property="fb:admins" content="' . $external_comments_conf['shortname'] . '" />'."\n";
+  }
+}
 
 /* frontend content replacement */
 function external_comments_display($contents) {
@@ -47,24 +61,38 @@ function external_comments_display($contents) {
 }
 
 /* echo the comment page code for use in templates */
-function get_external_comments() {
-  echo return_external_comments();
+function get_external_comments($PostID="", $PageURL="", $PageTitle="") {
+  echo return_external_comments($PostID, $PageURL, $PageTitle);
 }
 
 /* returns the appropriate page code */
-function return_external_comments() {
+function return_external_comments($PostID="", $PageURL="", $PageTitle="") {
   global $external_comments_conf;
+
+  if ($PostID=="")
+  {
+    $PostID=return_page_slug();
+  }
+  if ($PageURL=="")
+  {
+    $PageURL=get_page_url(True);
+  }
+  if ($PageTitle=="")
+  {
+    $PageTitle=return_page_title();
+  }
     
-  if ($external_comments_conf['provider'] == "Disqus")
-  {    
-    $new_content = '<div id="disqus_thread"></div>';
-    $new_content .= '<script type="text/javascript">';
-    $new_content .= "var disqus_shortname = '" . $external_comments_conf['shortname'] . "';"; 
-    $new_content .= "var disqus_developer = '" . $external_comments_conf['developer'] . "';"; 
-    $new_content .= "var disqus_identifier = '" . return_page_slug() . "';";
-    $new_content .= "var disqus_url = '" . get_page_url(True) . "';";
-    $new_content .= "var disqus_title = '" . return_page_title() . "';";
-    $new_content .= <<<INLINECODE
+  switch ($external_comments_conf['provider']) 
+  {
+    case "Disqus":    
+      $new_content = '<div id="disqus_thread"></div>';
+      $new_content .= '<script type="text/javascript">';
+      $new_content .= "var disqus_shortname = '" . $external_comments_conf['shortname'] . "';"; 
+      $new_content .= "var disqus_developer = '" . $external_comments_conf['developer'] . "';"; 
+      $new_content .= "var disqus_identifier = '" . $PostID . "';";
+      $new_content .= "var disqus_url = '" . $PageURL . "';";
+      $new_content .= "var disqus_title = '" . $PageTitle . "';";
+      $new_content .= <<<INLINECODE
     (function() {
         var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
         dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
@@ -74,16 +102,23 @@ function return_external_comments() {
 <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 <a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>
 INLINECODE;
-  }
-  elseif ($external_comments_conf['provider'] == "ID")
-  {
-    $new_content = '<script>';
-    $new_content .= "var idcomments_acct = '" . $external_comments_conf['shortname'] . "';";
-    $new_content .= "var idcomments_post_id = '" . return_page_slug() . "';";
-    $new_content .= "var idcomments_post_url = '" . get_page_url(True) . "';";
-    $new_content .= "</script>";
-    $new_content .= '<span id="IDCommentsPostTitle" style="display:none"></span>';
-    $new_content .= "<script type='text/javascript' src='http://www.intensedebate.com/js/genericCommentWrapperV2.js'></script>";
+      break;
+    
+    case "ID":
+      $new_content = '<script>';
+      $new_content .= "var idcomments_acct = '" . $external_comments_conf['shortname'] . "';";
+      $new_content .= "var idcomments_post_id = '" . $PostID . "';";
+      $new_content .= "var idcomments_post_url = '" . $PageURL . "';";
+      $new_content .= "</script>";
+      $new_content .= '<span id="IDCommentsPostTitle" style="display:none"></span>';
+      $new_content .= "<script type='text/javascript' src='http://www.intensedebate.com/js/genericCommentWrapperV2.js'></script>";
+      break;
+      
+    case "facebook":
+      $new_content = '<div id="fb-root"></div>';
+      $new_content .= '<script src="http://connect.facebook.net/en_US/all.js#appId=APP_ID&amp;xfbml=1"></script>';
+      $new_content .= '<fb:comments href="' . $PageURL . '" num_posts="" width=""></fb:comments>';
+      break;      
   }
   return $new_content;
 }
@@ -122,20 +157,32 @@ function external_comments_config() {
   echo '> Disqus<br />';
   echo '<input type="radio" name="provider" value="ID" ';
   if ($external_comments_conf['provider'] == "ID") echo "checked";
-  echo '> Intense Debate<br /></p>';
-  
-  echo '<p>Your forum\'s ID - the unique identifier for your website from the comment provider:<br />';
-  echo '(For Disqus, this is referred to as your <i>Forum Shortname</i>, for IntenseDebate this is referred to as your <i>Site Account</i>.)';
-  echo '<input name="shortname" type="text" size="90" value="'.$external_comments_conf['shortname'] .'"></p>';
-  
-  if ($external_comments_conf['provider'] == "Disqus")
-  {
-    echo '<input name="developer" type="checkbox" value="Y"';
-    if ($external_comments_conf['developer'] == 1) echo ' checked';
-    echo '>&nbsp; Developer Mode: testing the system on an inaccessible website, e.g. secured staging server or a local environment.<br /><br />';
+  echo '> Intense Debate<br />';
+  echo '<input type="radio" name="provider" value="facebook" ';
+  if ($external_comments_conf['provider'] == "facebook") echo "checked";
+  echo '> Facebook Comments<br /></p>';
+
+  echo "<input name='submit_settings' class='submit' type='submit' value='Update External Comment Provider'><br /><br /><br />";
+    
+  switch ($external_comments_conf['provider']) {
+    case "Disqus":
+      echo '<p>Your Disqus <i>Forum Shortname</i>';
+      echo '<input name="shortname" type="text" size="90" value="'.$external_comments_conf['shortname'] .'"></p>';      
+      echo '<p><input name="developer" type="checkbox" value="Y"';
+      if ($external_comments_conf['developer'] == 1) echo ' checked';
+      echo '>&nbsp; Developer Mode: testing the system on an inaccessible website, e.g. secured staging server or a local environment.</p><br />';
+      break;
+    case "ID":
+      echo '<p>Your IntenseDebate <i>Site Account</i>';
+      echo '<input name="shortname" type="text" size="90" value="'.$external_comments_conf['shortname'] .'"></p>';      
+      break;
+    case "facebook":
+      echo '<p>Your Facebook <i>UserID</i> (needed for moderation)';
+      echo '<input name="shortname" type="text" size="90" value="'.$external_comments_conf['shortname'] .'"></p>';      
+      break;
   }
-  
-  echo "<input name='submit_settings' class='submit' type='submit' value='Save Settings'>\n";
+      
+  echo "<input name='submit_settings' class='submit' type='submit' value='Save Settings'><br />";
   echo '</form>';
   echo '<p /><p><i>Enable comments on a single page by adding the tag <b>(% external_comments %)</b> in that page.<br />';
   echo 'Alternately insert <b>&lt?php get_external_comments(); ?&gt</b> in your page template to have comments for all pages.</i></p>';
